66b5054676ef8c3ec1c78882740c8ecb42ddba28
Uppaluru KiranKumar <UpKiranKumar@BLR.ALLEGISINDIA.COM>
2020-03-29 11:02:55 +0530

    added covid project

---
+++ covid-19-tracker/src/test/java/com/covi19/covid19/tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covi19.covid19.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
